8d1f664d046bfeb1a92d3f28626dba093c1d9bef
3D ALGO LAB <threealgo@outlook.com>
2024-02-16 11:43:12 +0900

    black applied

--- batch_gen.py
+++ batch_gen.py
@@ -2,11 +2,19 @@ import subprocess
 
 start_i = 0
 end_i = 49
-target_list=list(range(start_i, end_i +1))
+target_list = list(range(start_i, end_i + 1))
 # target_list = [1, 2, 3, 49]
 
 for i in target_list:
     # print(i)
-    # subprocess.run(["ls", "-l", f"/data/episode_{i}.hdf5"]) 
-    subprocess.run(["python3", "visualize_episodes.py", "--dataset_dir", "./data", 
-                    "--episode_idx", f"{i}"])
+    # subprocess.run(["ls", "-l", f"/data/episode_{i}.hdf5"])
+    subprocess.run(
+        [
+            "python3",
+            "visualize_episodes.py",
+            "--dataset_dir",
+            "./data",
+            "--episode_idx",
+            f"{i}",
+        ]
+    )
